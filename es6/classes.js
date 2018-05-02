6473011afa80692dd762dd18643eb24d94ea4477
Nikolai Yakuschenko <nikolai.yakuschenko@gmail.com>
2018-05-02 16:58:38 +0300

    completed #25.Testing the App Component

---
+++ es6/classes.js
@@ -0,0 +1,32 @@
+class Animal {
+    constructor(name, color) {
+        this.name = name;
+        this.color = color;
+    }
+
+    speak() {
+        // console.log('Hi, I\'m ' + this.name + ' and I\'m ' + this.color);
+        console.log(`Hi, I'm $(this.name}, and I'm ${this.color}`);
+    }
+}
+
+
+
+class Lion extends Animal {
+    constructor(name, color, role, home) {
+        super(name, color);
+
+        this.role = role;
+        this.home = home;
+    }
+
+    roar() {
+        console.log(`I'm the ${this.role} of ${this.home}`);
+    }
+}
+
+let lion = new Lion('Mufasa', 'golden', 'king', 'Pride rock');
+let lion_2 = new Lion('Scar', 'maroon', 'outcast', 'the shadow land');
+
+lion.roar();
+lion_2.roar();
---
+++ notetoself/README.md
@@ -0,0 +1,5 @@
+React, Redux, & Enzyme - Introducing Apps & Tests
+
+https://coursehunters.net/course/react-redux-enzyme-znakomstvo-s-prilozheniyami-i-testami
+
+02.05.2018 - completed #25.Testing the App Component
